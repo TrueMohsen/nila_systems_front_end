bc8788605ee869708b438d89bcdbb6c95b445102
mohsen <seaoceania4@gmail.com>
2026-01-04 22:14:23 +0330

    main: add a code to remove __dirname

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/** Fix for __dirname in ESM (Vercel build) */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const cspHeader = `
     default-src 'self';
